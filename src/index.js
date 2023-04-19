bc5311332ab3eda0a0a900393cda42aeb387b42d
Olivia Desgrossillier <oliviadesgrossillier@gmail.com>
2023-04-18 21:36:44 -0700

    completed tasklister: may come back for challenges

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    createTask(document.querySelector('#new-task-description').value) 
+    form.reset()
+  })
 });
+
+function createTask(newTask){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'Finished!'
+  p.textContent = `${newTask} `
+  p.appendChild(btn)
+  document.querySelector('#list').appendChild(p)
+}
+
+function handleDelete(event) {
+  event.target.parentNode.remove()
+}
+
